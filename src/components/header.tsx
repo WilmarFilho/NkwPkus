328c2d123b1d2b338309514b2e6b353a4db6dd20
Wilmar José Alves Ferreira Filho <wilmarfilho32@hotmail.com>
2025-04-23 00:15:56 -0300

    Css Responsividade no Header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,20 +1,35 @@
+import { useState } from 'react';
 import logo from './assets/logo.svg';
-import './assets/index.css'
+import './assets/index.css';
 
 export default function Header() {
-    return (
-        <header className='row'>
-            <div  className='col-3 wrapperLogo'> <a href='https://nkw.framer.website'> <img src={logo}></img> </a></div>
-            <article className='col-9 navHeader'>
-                <ul>
-                    <li><a href='https://nkw.framer.website/#hero'>Início</a></li>
-                    <li><a href='https://nkw.framer.website/#fundadores'>Fundadores</a></li>
-                    <li><a href='https://nkw.framer.website/#solucoes'>Soluções</a></li>
-                    <li><a href='https://nkw.framer.website/#valores'>Valores</a></li>
-                    <li><a href='https://nkw.framer.website/#nkw+'>NKW+</a></li>
-                    <li><a className='btnContato'>Fale Conosco</a></li>
-                </ul>
-            </article>
-        </header>
-    )
+  const [menuAberto, setMenuAberto] = useState(false);
+
+  return (
+    <header className={`row header ${menuAberto ? 'ativo' : ''}`}>
+      <div className="col-8  col-md-3 col-lg-3 wrapperLogo">
+        <a href="https://nkw.framer.website">
+          <img src={logo} alt="Logo" />
+        </a>
+      </div>
+
+      {/* Botão Hamburguer */}
+      <div className={`col-4 col-md-9 col-lg-9 menu-toggle ${menuAberto ? 'ativo' : ''}`} onClick={() => setMenuAberto(!menuAberto)}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      <article className={`col-8 col-md-9 col-lg-9 navHeader ${menuAberto ? 'ativo' : ''}`}>
+        <ul>
+          <li><a href="https://nkw.framer.website/#hero">Início</a></li>
+          <li><a href="https://nkw.framer.website/#fundadores">Fundadores</a></li>
+          <li><a href="https://nkw.framer.website/#solucoes">Soluções</a></li>
+          <li><a href="https://nkw.framer.website/#valores">Valores</a></li>
+          <li><a href="https://nkw.framer.website/#nkw+">NKW+</a></li>
+          <li><a href="https://wa.link/ej8dzw" className="btnContato">Fale Conosco</a></li>
+        </ul>
+      </article>
+    </header>
+  );
 }
